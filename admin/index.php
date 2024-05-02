12f97d70f7175cd9822edb661b4ecef60b029c89
farsewin <bouzianefaris07@gmail.com>
2024-05-02 19:36:10 +0100

    admin dashboard

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+    </head>
+
+    <body>
+        <div class="menu">
+            Menu Goes Here
+        </div>
+
+        <div class="main">
+            Main Content Goes Here
+        </div>
+
+        <div class="footer">
+            Footer Goes Here
+        </div>
+        
+    </body>
+</html>
