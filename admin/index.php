0f331c7dd9cc61153e7fe1552c325004f80ee4c8
farsewin <bouzianefaris07@gmail.com>
2024-05-02 19:58:27 +0100

    version 1.1

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,17 @@
 <html>
     <head>
         <title>Food Order Website - Home Page</title>
+
+        <link rel="stylesheet" type="text/css" href="../css/admin.css">
+        <!-- Link Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
     </head>
 
     <body>
         <div class="menu">
-            Menu Goes Here
+            <div class="wrapper">Menu Goes Here</div>
         </div>
 
         <div class="main">
