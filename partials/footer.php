0520f48561b27de664f86260c94636793078225e
farsewin <bouzianefaris07@gmail.com>
2024-05-04 13:25:47 +0100

    partials

---
+++ partials/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+        <div class="wrapper">
+            <p class="text-center">All rights reserved. Designed By <a href="#">Mohamed and Faris</a></p>
+
+    </div>    
+        </div>
+        
+    </body>
+</html>
