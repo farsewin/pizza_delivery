c12e2dcf8374782a69b433effb663c6ad249fd79
farsewin <bouzianefaris07@gmail.com>
2024-05-04 13:24:52 +0100

    refactored by <admin>

---
+++ admin/manage-admin.php
@@ -0,0 +1,42 @@
+<?php require '../partials/head.php';?>
+
+<?php require '../partials/nav.php';?>
+
+        <div class="main">
+        <div class="wrapper">
+            <h1>Manage Admin</h1>
+
+            <table class="tbl-full">
+                <tr>
+                    <th>I.D.</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Action</th>
+                </tr>
+
+                <tr>
+                    <td>1.</td>
+                    <td>Mohamed</td>
+                    <td>Mohamed28</td>
+                    <td>Update delete</td>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>Mohamed</td>
+                    <td>Mohamed28</td>
+                    <td>Update delete</td>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>Mohamed</td>
+                    <td>Mohamed28</td>
+                    <td>Update delete</td>
+                </tr>
+            </table>
+
+           
+
+        </div> 
+    </div>
+
+    <?php require '../partials/footer.php';?>
---
+++ admin/manage-order.php
@@ -0,0 +1,14 @@
+<?php require '../partials/head.php';?>
+
+<?php require '../partials/nav.php';?>
+
+        <div class="main">
+        <div class="wrapper">
+            <h1>Manage Order</h1>
+
+           
+
+        </div> 
+    </div>
+
+    <?php require '../partials/footer.php';?>
