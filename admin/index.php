427315d97f02921c6a1c0921330ab9aada9d2cb4
farsewin <bouzianefaris07@gmail.com>
2024-05-04 13:23:42 +0100

    view

--- admin/index.php
+++ admin/index.php
@@ -1,26 +1,38 @@
-<html>
-    <head>
-        <title>Food Order Website - Home Page</title>
+<?php require '../partials/head.php';?>
 
-        <link rel="stylesheet" type="text/css" href="../css/admin.css">
-        <!-- Link Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
-    </head>
-
-    <body>
-        <div class="menu">
-            <div class="wrapper">Menu Goes Here</div>
-        </div>
+<?php require '../partials/nav.php';?>
 
         <div class="main">
-            Main Content Goes Here
-        </div>
+        <div class="wrapper">
+            <h1>Dashboard</h1>
 
-        <div class="footer">
-            Footer Goes Here
-        </div>
-        
-    </body>
-</html>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="clearfix"></div>
+
+        </div> 
+    </div>
+
+    <?php require '../partials/footer.php';?>
